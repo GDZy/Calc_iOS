04fd1e781dff9bb5cf03a2739b8893b4f28fae55
-= <=->
2018-09-14 18:02:19 +0300

    end task 1.1

--- Calc/ViewController.swift
+++ Calc/ViewController.swift
@@ -9,32 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     
     @IBOutlet weak var dysplay: UILabel!
+    @IBOutlet weak var history: UILabel!
+    @IBOutlet weak var decimalSeparator: UIButton!
+    
     var userIsInTheTypingOfMiddleANumber: Bool = false
     var operandStack = Array<Double>()
     var dysplayValue: Double {
         set {
-            dysplay.text = "\(newValue)"
+            dysplay.text = numberFormatter().string(from: NSNumber(value: newValue))!
             userIsInTheTypingOfMiddleANumber = false
         }
         get {
-            return NumberFormatter().number(from: dysplay.text!)!.doubleValue
+            return numberFormatter().number(from: dysplay.text!)!.doubleValue
+    
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        decimalSeparator.setTitle(numberFormatter().decimalSeparator, for: .normal)
+        dysplayValue = 0
+        history.text = " "
+        operandStack = []
     }
     
     @IBAction func appdendDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
         if userIsInTheTypingOfMiddleANumber {
-            dysplay.text = dysplay.text! + digit
+            if !(digit == decimalSeparator.currentTitle! && (dysplay.text?.contains(decimalSeparator.currentTitle!))!) {
+                dysplay.text = dysplay.text! + digit
+            }
         } else {
             dysplay.text = digit
             userIsInTheTypingOfMiddleANumber = true
+        }
+    }
+    
+
+    @IBAction func removeLastDigit() {
+        dysplay.text = String((dysplay.text?.dropLast())!)
+        if dysplay.text == "" {
+            dysplay.text = " "
         }
     }
     
@@ -43,36 +60,77 @@ class ViewController: UIViewController {
             enter()
         }
         let operation = sender.currentTitle!
+        addHistory(text: operation)
         switch operation {
-        case "×": preformOperation { $0 * $1 }
-        case "÷": preformOperation { $1 / $0 }
-        case "+": preformOperation { $0 + $1 }
-        case "-": preformOperation { $1 - $0 }
-        case "√": preformOperation { sqrt($0) }
+        case "×": pereformOperation { $0 * $1 }
+        case "÷": pereformOperation { $1 / $0 }
+        case "+": pereformOperation { $0 + $1 }
+        case "-": pereformOperation { $1 - $0 }
+        case "sin": pereformOperation { sin($0) }
+        case "cos": pereformOperation { cos($0) }
+        case "sqrt": pereformOperation { sqrt($0) }
+        case "𝜋": performOperation { Double.pi }
         default:
             break
         }
-        
     }
     
-    private func preformOperation(operation: (Double, Double) -> Double)  {
-        if operandStack.count >= 2 {
-            dysplayValue = operation(operandStack.removeLast(), operandStack.removeLast())
-            enter()
+    @nonobjc private func performOperation(operation: () -> Double) {
+        dysplayValue = operation()
+        addStack()
+    }
+    
+    private func pereformOperation(operation: (Double) -> Double) {
+        if operandStack.count >= 1 {
+            dysplayValue = operation(operandStack.removeLast())
+            addStack()
         }
     }
     
-    private func preformOperation(operation: (Double) -> Double) {
-        if operandStack.count >= 1 {
-            dysplayValue = operation(operandStack.removeLast())
-            enter()
+    private func pereformOperation(operation: (Double, Double) -> Double)  {
+        if operandStack.count >= 2 {
+            dysplayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            addStack()
         }
     }
     
     @IBAction func enter() {
+        addHistory(text: dysplay.text!)
+        
         userIsInTheTypingOfMiddleANumber = false
+        
+        addStack()
+    }
+    
+    @IBAction func reset(_ sender: UIButton) {
+        viewDidLoad()
+    }
+    
+    private func addStack() {
         operandStack.append(dysplayValue)
         print("stack = \(operandStack)")
     }
+    
+    private func numberFormatter() -> NumberFormatter {
+        let numberFormatterLoc = NumberFormatter()
+        numberFormatterLoc.numberStyle = .decimal
+        numberFormatterLoc.maximumIntegerDigits = 5
+        numberFormatterLoc.notANumberSymbol = "Error"
+        numberFormatterLoc.groupingSeparator = " "
+        return numberFormatterLoc
+    }
+    
+    private func addHistory(text: String) {
+        history.text = history.text! + " " + text
+    }
 }
 
+
+//extension UIButton {
+//    override open func layoutSubviews() {
+//        super.layoutSubviews()
+//        self.layer.borderColor = UIColor.black.cgColor
+//        self.layer.cornerRadius = 3
+//        self.layer.borderWidth = 1
+//    }
+//}
