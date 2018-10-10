736504601ae5dc0a84c70f04158b58c09cf1bd95
-= <=->
2018-10-10 10:40:58 +0300

    [t-1.4] - starting

---
+++ Calc/CalculatorBrain.swift
@@ -0,0 +1,11 @@
+//
+//  CalculatorBrain.swift
+//  Calc
+//
+//  Created by Dzmitry Herasiuk on 09.10.2018.
+//  Copyright © 2018 Dzmitry Herasiuk. All rights reserved.
+//
+
+import Foundation {
+    
+}
